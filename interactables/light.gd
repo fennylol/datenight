4b2b7ecbaa83cfa59bbd5ef11219ec49fa8dad49
David Fenneran <148485725+erraticandunplanned@users.noreply.github.com>
2026-07-25 00:19:20 -0700

    a bit of a tutorial

--- interactables/light.gd
+++ interactables/light.gd
@@ -1,13 +1,16 @@
 extends Interactable
 class_name Light
 
-var Powered: bool = true
+@export var Powered: bool = false
 var Power: float = 0.0
 
 @onready var Bulb: Light3D = $Bulb
 
 func _ready() -> void:
    Power = Bulb.light_energy
+   if not Powered:
+      Bulb.visible = Powered
+      Bulb.light_energy = Power if Powered else 0.0
 
 func on_interact() -> void:
    Powered = not Powered
--- the_home/the_home.gd
+++ the_home/the_home.gd
@@ -3,8 +3,12 @@ extends Node3D
 @onready var THE_CHILD: TheChild = $TheChild
 @onready var THE_CREATURE: TheCreature = $TheCreature
 @onready var ROOMS: Node3D = $NavigationRegion3D/Rooms
+@onready var INTERFACE: Control = $Interface
+@onready var WORLDPROMPTS : Node3D = $WorldPrompts
 
 var last_room: Room
+var game_time : float = 0.0
+var tutorial_level : int = 0
 
 func _get_containing_room() -> Room:
    for room:Room in ROOMS.get_children():
@@ -12,10 +16,41 @@ func _get_containing_room() -> Room:
          return room
    return null
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+   game_time += delta
+   _attempt_tutorial(delta)
    THE_CREATURE.set_movement_target(THE_CHILD.position)
    
    var closest_room: Room = _get_containing_room()
    if closest_room != last_room:
       print(closest_room.RoomName if closest_room else "idk where you are LMAO")
       last_room = closest_room
+
+func _attempt_tutorial(delta):
+   if tutorial_level == 0:
+      ## WAKE UP ANIMATION
+      var wake_speed = 0 if game_time < 0.5 else 100 if game_time < 1.0 else 500 if game_time < 2.0 else -2700 if game_time < 2.25 else 2900
+      var wake_up = INTERFACE.get_child(0)
+      wake_up.get_child(0).position.y -= delta * wake_speed
+      wake_up.get_child(1).position.y += delta * wake_speed
+      if game_time > 3.0: 
+         wake_up.visible = false
+         tutorial_level = 1
+   elif tutorial_level == 1:
+      var lights = INTERFACE.get_child(1)
+      if game_time < 4.0: return
+      elif game_time < 7.0:
+         lights.visible = true
+      else:
+         lights.visible = false
+      if Input.is_action_just_pressed("interact"):
+         WORLDPROMPTS.get_child(0).visible = false
+         game_time = 0.0
+         tutorial_level = 2
+   elif tutorial_level == 2:
+      var lights = INTERFACE.get_child(1)
+      lights.visible = true
+      lights.get_child(0).text = "ALL OF THEM"
+      if game_time > 3.0:
+         lights.visible = false
+         tutorial_level = 3
